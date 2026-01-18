f17f0f43f513ac52be270058611a35d0df8f98ff
Shubham Bhatt <shubhambhatt92021@gmail.com>
2026-01-18 17:13:57 +0530

    Implement CircleShape base class and Player class with drawing functionality

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state 
+from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock_object = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -13,11 +18,17 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock_object.tick(60) / 1000
+        print(dt)
+
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
